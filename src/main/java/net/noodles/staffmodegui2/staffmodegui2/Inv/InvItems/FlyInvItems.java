bc39b96f7d3d29f2bfe4f27a62860556a3621e74
ZachstylesDevelopment <31602472+ZachstylesDevelopment@users.noreply.github.com>
2018-09-19 13:36:07 -0500

    Create FlyInvItems.java

---
+++ src/main/java/net/noodles/staffmodegui2/staffmodegui2/Inv/InvItems/FlyInvItems.java
@@ -0,0 +1,30 @@
+package net.noodles.staffmodegui2.staffmodegui2.Inv.InvItems;
+
+import net.noodles.staffmodegui2.staffmodegui2.StaffModeGUI2;
+import net.noodles.staffmodegui2.staffmodegui2.util.ItemBuilder;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
+
+public class FlyInvItems {
+
+    public static ItemStack FlyON() {
+        return new ItemBuilder(Material.EMERALD_BLOCK)
+                .setName(StaffModeGUI2.getPlugin().getConfig().getString("flyMenu.titleItemON").replace("&", "§"))
+                .setLore(Arrays.asList(
+                        StaffModeGUI2.getPlugin().getConfig().getString("flyMenu.loreItemON").replace("&", "§")
+                ))
+                .toItemStack();
+    }
+
+    public static ItemStack FlyOFF() {
+        return new ItemBuilder(Material.REDSTONE_BLOCK)
+                .setName(StaffModeGUI2.getPlugin().getConfig().getString("flyMenu.titleItemOFF").replace("&", "§"))
+                .setLore(Arrays.asList(
+                        StaffModeGUI2.getPlugin().getConfig().getString("flyMenu.loreItemOFF").replace("&", "§")
+                ))
+                .toItemStack();
+    }
+
+}
