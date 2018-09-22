82d708cea84bf78acc1c00b60ee92a326d2ca986
BuildTools <unconfigured@null.spigotmc.org>
2018-09-22 09:00:50 -0700

    Dev Command Updates

--- src/main/java/net/noodles/staffmodegui2/staffmodegui2/util/Settings.java
+++ src/main/java/net/noodles/staffmodegui2/staffmodegui2/util/Settings.java
@@ -8,7 +8,7 @@ public class Settings {
     public static String DEVELOPER_URL = "https://bghddevelopment.com";
     public static String DEV_MESSAGE = ChatColor.RED + "This command can only be used by BGHDDevelopment for support reasons.";
     public static String DEV_MESSAGE2 = ChatColor.GRAY + "If you are worried about what this command does check it out on GitHub here:";
-    public static String DEV_MESSAGE3 = ChatColor.GRAY + "";
+    public static String DEV_MESSAGE3 = ChatColor.GRAY + "https://goo.gl/87ZgJz";
     public static String PLUGIN_URL = "https://spigotmc.org/resources/60960";
     public static String SUPPORT_DISCORD_URL = "https://discord.gg/VB8kB8P";
     public static String VERSION = "0.8";
