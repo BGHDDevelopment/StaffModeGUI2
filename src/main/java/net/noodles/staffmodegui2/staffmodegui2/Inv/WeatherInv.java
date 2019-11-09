ff9a6b6eff91847c827fe060a37995be045c8c16
Refrac <31602472+RefracDevelopment@users.noreply.github.com>
2019-11-09 17:56:03 -0600

    Changed mainMenuReturn more details below
    The return button now returns you to the previous menu you were in instead of just sending you back to the Main Menu

--- src/main/java/net/noodles/staffmodegui2/staffmodegui2/Inv/WeatherInv.java
+++ src/main/java/net/noodles/staffmodegui2/staffmodegui2/Inv/WeatherInv.java
@@ -1,13 +1,10 @@
 package net.noodles.staffmodegui2.staffmodegui2.Inv;
 
-import net.noodles.staffmodegui2.staffmodegui2.Inv.InvItems.MainInvItems;
-import net.noodles.staffmodegui2.staffmodegui2.Inv.InvItems.TimeInvItems;
-import net.noodles.staffmodegui2.staffmodegui2.Inv.InvItems.WeatherInvItems;
+import net.noodles.staffmodegui2.staffmodegui2.Inv.InvItems.WhitelistInvItems;
 import net.noodles.staffmodegui2.staffmodegui2.StaffModeGUI2;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,17 +14,17 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 
-public class WeatherInv implements Listener {
+public class WhitelistInv implements Listener {
 
     private StaffModeGUI2 main;
 
-    public WeatherInv(StaffModeGUI2 main) {
+    public WhitelistInv(StaffModeGUI2 main) {
         this.main = main;
         main.getServer().getPluginManager().registerEvents(this, main);
     }
 
     private String getTitle() {
-        return ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "Weather Control";
+        return ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "Whitelist Control";
     }
 
     private int getSize() {
@@ -36,10 +33,9 @@ public class WeatherInv implements Listener {
     public Inventory getInventory() {
         Inventory inv = Bukkit.createInventory(null, getSize(), getTitle());
 
-        inv.setItem(2, WeatherInvItems.weatherClear());
-        inv.setItem(6, WeatherInvItems.weatherStorm());
-        inv.setItem(8, MainInvItems.mainMenuReturn());
-
+        inv.setItem(2, WhitelistInvItems.whitelistON());
+        inv.setItem(6, WhitelistInvItems.whitelistOFF());
+        inv.setItem(8, WhitelistInvItems.menuReturn());
 
         for (int i = 0; i < 9; ++i) {
             if (inv.getItem(i) == null) {
@@ -71,19 +67,17 @@ public class WeatherInv implements Listener {
         if (event.getCurrentItem().getType() == Material.AIR) return;
 
         event.setCancelled(true);
-        if (event.getCurrentItem().isSimilar(WeatherInvItems.weatherClear())) {
-            player.sendMessage(StaffModeGUI2.getPlugin().getConfig().getString("weatherMenu.weatherClearMessage").replace("&", "§"));
+        if (event.getCurrentItem().isSimilar(WhitelistInvItems.whitelistON())) {
+            Bukkit.getServer().setWhitelist(true);
+            player.sendMessage(StaffModeGUI2.getPlugin().getConfig().getString("whitelistMenu.messageItemON").replace("&", "§"));
             player.closeInventory();
-            World world = player.getWorld();
-            world.setStorm(false);
-        } else if (event.getCurrentItem().isSimilar(WeatherInvItems.weatherStorm())) {
-            player.sendMessage(StaffModeGUI2.getPlugin().getConfig().getString("weatherMenu.weatherStormMessage").replace("&", "§"));
+        } else if (event.getCurrentItem().isSimilar(WhitelistInvItems.whitelistOFF())) {
+            Bukkit.getServer().setWhitelist(false);
+            player.sendMessage(StaffModeGUI2.getPlugin().getConfig().getString("whitelistMenu.messageItemOFF").replace("&", "§"));
             player.closeInventory();
-            World world = player.getWorld();
-            world.setStorm(true);
-        } else if (event.getCurrentItem().isSimilar(MainInvItems.mainMenuReturn())) {
-            player.sendMessage(StaffModeGUI2.getPlugin().getConfig().getString("mainMenuReturn.message").replace("&", "§"));
-            player.openInventory(StaffModeGUI2.getInstance().getMainInv().getInventory());
+        } else if (event.getCurrentItem().isSimilar(WhitelistInvItems.menuReturn())) {
+            player.sendMessage(StaffModeGUI2.getPlugin().getConfig().getString("mainMenuReturn.message1").replace("&", "§"));
+            player.openInventory(StaffModeGUI2.getInstance().getServerManagerInv().getInventory());
 
         }
     }
