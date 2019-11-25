2d7761908affe50809129554a32ac743652c0b3f
Refrac <31602472+RefracDevelopment@users.noreply.github.com>
2019-11-25 04:42:09 -0600

    Updated the Update Checker config option method

--- src/main/java/net/noodles/staffmodegui2/staffmodegui2/Events/UpdateNotification.java
+++ src/main/java/net/noodles/staffmodegui2/staffmodegui2/Events/UpdateNotification.java
@@ -12,8 +12,6 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 public class UpdateNotification implements Listener {
 
-
-    public UpdateChecker checker;
     private StaffModeGUI2 main;
 
     public UpdateNotification(StaffModeGUI2 main) {
@@ -25,13 +23,13 @@ public class UpdateNotification implements Listener {
     public void onJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
         if (p.hasPermission("staffmodegui.update")) {
-            if (StaffModeGUI2.getPlugin().getConfig().getBoolean("Update.Enabled") == true){
-                this.checker = new UpdateChecker(StaffModeGUI2.getPlugin());
-                if (this.checker.isConnected()) {
-                    if (this.checker.hasUpdate()) {
+            if (StaffModeGUI2.getPlugin().getConfig().getBoolean("Update.Enabled")){
+                UpdateChecker checker = new UpdateChecker ( StaffModeGUI2.getPlugin () );
+                if (checker.isConnected()) {
+                    if (checker.hasUpdate()) {
                         p.sendMessage(ChatColor.GRAY + "********************");
                         p.sendMessage(ChatColor.RED + "StaffModeGUI2 is outdated!");
-                        p.sendMessage(ChatColor.GREEN + "Newest version: " + this.checker.getLatestVersion());
+                        p.sendMessage(ChatColor.GREEN + "Newest version: " + checker.getLatestVersion());
                         p.sendMessage(ChatColor.RED + "Your version: " + Settings.VERSION);
                         p.sendMessage(ChatColor.GRAY + "********************");
                     }
