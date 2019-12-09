42fabce7d8ba5b2f5df61cbb822b2c5521ec18a8
Refrac <31602472+RefracDevelopment@users.noreply.github.com>
2019-12-08 22:19:01 -0600

    Fixed the MainInv it was the main class sorry about that Stephan/Noodles

--- src/main/java/net/noodles/staffmodegui2/staffmodegui2/Inv/MainInv.java
+++ src/main/java/net/noodles/staffmodegui2/staffmodegui2/Inv/MainInv.java
@@ -1,211 +1,115 @@
-package net.noodles.staffmodegui2.staffmodegui2;
+package net.noodles.staffmodegui2.staffmodegui2.Inv;
 
-import net.noodles.staffmodegui2.staffmodegui2.commands.StaffModeCommand;
-import net.noodles.staffmodegui2.staffmodegui2.commands.StaffModeDev;
-import net.noodles.staffmodegui2.staffmodegui2.Inv.*;
-import net.noodles.staffmodegui2.staffmodegui2.util.Logger;
-import net.noodles.staffmodegui2.staffmodegui2.util.MetricsLite;
-import net.noodles.staffmodegui2.staffmodegui2.util.Settings;
-import net.noodles.staffmodegui2.staffmodegui2.util.UpdateChecker.UpdateChecker;
+import net.noodles.staffmodegui2.staffmodegui2.Inv.InvItems.MainInvItems;
+import net.noodles.staffmodegui2.staffmodegui2.StaffModeGUI2;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.java.JavaPlugin;
-import java.io.File;
-import java.util.Arrays;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
-public final class StaffModeGUI2 extends JavaPlugin {
+public class MainInv implements Listener {
 
-    public static StaffModeGUI2 plugin;
-    private UpdateChecker checker;
-    private static StaffModeGUI2 instance;
+    private StaffModeGUI2 main;
 
+    public MainInv(StaffModeGUI2 main) {
+        this.main = main;
+        main.getServer ().getPluginManager ().registerEvents ( this , main );
+    }
 
-    //INV
-    private MainInv mainInv;
-    private WhitelistInv whitelistInv;
-    private TimeInv timeInv;
-    private WeatherInv weatherInv;
-    private GamemodeInv gamemodeInv;
-    private FlyInv flyInv;
-    private VanishInv vanishInv;
-    private MobInv mobInv;
-    private ExtraMobInv extraMobInv;
-    private TrollInv trollInv;
-    private ToolsInv toolsInv;
-    private ServerManagerInv serverManagerInv;
-    private FeedAndHealInv feedAndHealInv;
-    private EffectsInv effectsInv;
-    private SpeedInv speedInv;
-    private StrengthInv strengthInv;
-    private JumpboostInv jumpboostInv;
+    private String getTitle() {
+        return ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "StaffModeGUI";
+    }
 
-    @Override
-    public void onEnable() {
-        Logger.log ( Logger.LogLevel.OUTLINE , "********************" );
-        Logger.log ( Logger.LogLevel.INFO , "Initializing StaffModeGUI2 Version: " + Settings.VERSION );
-        Logger.log ( Logger.LogLevel.INFO , "Created by: " + Settings.DEVELOPER_NAME );
-        Logger.log ( Logger.LogLevel.INFO , "Website: " + Settings.DEVELOPER_URL );
-        Logger.log ( Logger.LogLevel.INFO , "Spigot Link: " + Settings.PLUGIN_URL );
-        Logger.log ( Logger.LogLevel.INFO , "Support Link: " + Settings.SUPPORT_DISCORD_URL );
-        Logger.log ( Logger.LogLevel.OUTLINE , "********************" );
-        Logger.log ( Logger.LogLevel.INFO , "Plugin Loading..." );
-        Logger.log ( Logger.LogLevel.INFO , "Registering Managers..." );
-        this.plugin = this;
-        instance = this;
-        MetricsLite metrics = new MetricsLite ( this );
-        Logger.log ( Logger.LogLevel.INFO , "Managers Registered!" );
-        Logger.log ( Logger.LogLevel.INFO , "Registering Listeners..." );
-        registerListener (
-                //NOT NEEDED
-        );
-        Logger.log ( Logger.LogLevel.INFO , "Listeners Registered!" );
-        Logger.log ( Logger.LogLevel.INFO , "Registering Commands..." );
-        new StaffModeCommand ();
-        new StaffModeDev ();
-        Logger.log ( Logger.LogLevel.INFO , "Commands Registered!" );
-        Logger.log ( Logger.LogLevel.INFO , "Loading Inventory's..." );
-        this.mainInv = new MainInv ( this );
-        this.whitelistInv = new WhitelistInv ( this );
-        this.timeInv = new TimeInv ( this );
-        this.weatherInv = new WeatherInv ( this );
-        this.gamemodeInv = new GamemodeInv ( this );
-        this.flyInv = new FlyInv ( this );
-        this.vanishInv = new VanishInv ( this );
-        this.mobInv = new MobInv ( this );
-        this.extraMobInv = new ExtraMobInv ( this );
-        this.trollInv = new TrollInv ( this );
-        this.toolsInv = new ToolsInv ( this );
-        this.serverManagerInv = new ServerManagerInv ( this );
-        this.feedAndHealInv = new FeedAndHealInv ( this );
-        this.effectsInv = new EffectsInv(this);
-        this.speedInv = new SpeedInv ( this );
-        this.strengthInv = new StrengthInv ( this );
-        this.jumpboostInv = new JumpboostInv ( this );
-        Logger.log ( Logger.LogLevel.INFO , "Inventory's Registered!" );
-        Logger.log ( Logger.LogLevel.INFO , "Loading Config's..." );
-        this.createConfig ();
-        Logger.log ( Logger.LogLevel.INFO , "Config's Registered!" );
-        Logger.log ( Logger.LogLevel.SUCCESS , "StaffModeGUI2 Version: " + Settings.VERSION + " Loaded." );
-        this.setEnabled ( true );
-        Logger.log ( Logger.LogLevel.OUTLINE , "********************" );
-        Logger.log ( Logger.LogLevel.INFO , "Checking for updates..." );
-        this.checker = new UpdateChecker ( this );
-        if (this.checker.isConnected ()) {
-            if (this.checker.hasUpdate ()) {
-                Logger.log ( Logger.LogLevel.OUTLINE , "********************" );
-                Logger.log ( Logger.LogLevel.WARNING , ("StaffModeGUI2 is outdated!") );
-                Logger.log ( Logger.LogLevel.WARNING , ("Newest version: " + this.checker.getLatestVersion ()) );
-                Logger.log ( Logger.LogLevel.WARNING , ("Your version: " + Settings.VERSION) );
-                Logger.log ( Logger.LogLevel.WARNING , ("Please Update Here: " + Settings.PLUGIN_URL) );
-                Logger.log ( Logger.LogLevel.OUTLINE , "********************" );
-            } else {
-                Logger.log ( Logger.LogLevel.SUCCESS , "StaffModeGUI2 is up to date!" );
+    private int getSize() {
+        return 54;
+    }
+
+    public Inventory getInventory() {
+        Inventory inv = Bukkit.createInventory ( null , getSize () , getTitle () );
+
+        inv.setItem ( 11 , MainInvItems.timeMenu () );
+        inv.setItem ( 13 , MainInvItems.gamemodeMenu () );
+        inv.setItem ( 15 , MainInvItems.mobMenu () );
+        inv.setItem ( 29 , MainInvItems.trollMenu () );
+        inv.setItem ( 31 , MainInvItems.toolsMenu () );
+        inv.setItem ( 33 , MainInvItems.serverManagerMenu () );
+        inv.setItem ( 47 , MainInvItems.effectsMenu () );
+        //inv.setItem(49, MainInvItems.Menu());
+        for (int i = 0; i < 54; ++i) {
+            if (inv.getItem ( i ) == null) {
+                inv.setItem ( i , Glass () );
             }
         }
+        return inv;
     }
 
 
-    @Override
-    public void onDisable() {
+    private ItemStack Glass() {
+        ItemStack stone = new ItemStack ( Material.STAINED_GLASS_PANE , 1 , (short) 8 );
+        ItemMeta stonem = stone.getItemMeta ();
+        stonem.setDisplayName ( "" );
+        stone.setItemMeta ( stonem );
+        return stone;
     }
 
 
-    private void registerListener(Listener... listeners) {
-        Arrays.stream ( listeners ).forEach ( l -> getServer ().getPluginManager ().registerEvents ( l , this ) );
-    }
-
-
-    @SuppressWarnings({"rawtypes" , "unchecked"})
-    public static StaffModeGUI2 getPlugin() {
-        return (StaffModeGUI2) getPlugin ( (Class) StaffModeGUI2.class );
-    }
-
-    private void createConfig() {
-        try {
-            if (!getDataFolder ().exists ()) getDataFolder ().mkdirs ();
-
-            File file = new File ( getDataFolder () , "config.yml" );
-            if (!file.exists ()) {
-                saveDefaultConfig ();
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        Player player = (Player) event.getWhoClicked ();
+        if (event.getClickedInventory () == null) return;
+        if (!event.getClickedInventory ().getTitle ().equals ( getTitle () )) return;
+        if (event.getCurrentItem () == null) return;
+        if (event.getCurrentItem ().getType () == Material.AIR) return;
+        event.setCancelled ( true );
+        // CLICK EVENTS
+        if (event.getCurrentItem ().isSimilar ( MainInvItems.timeMenu () )) {
+            if (!player.hasPermission ( "staffmodegui.timemenu" )) {
+                player.sendMessage ( StaffModeGUI2.getPlugin ().getConfig ().getString ( "defaultMessage.noPermission" ).replace ( "&" , "§" ) );
             }
-
-        } catch (Exception exception) {
-            getLogger ().info ( exception.getMessage () );
+            player.sendMessage ( StaffModeGUI2.getPlugin ().getConfig ().getString ( "timeMenu.openGUI" ).replace ( "&" , "§" ) );
+            player.openInventory ( StaffModeGUI2.getInstance ().getTimeInv ().getInventory () );
+        } else if (event.getCurrentItem ().isSimilar ( MainInvItems.gamemodeMenu () )) {
+            if (!player.hasPermission ( "staffmodegui.gamemodemenu" )) {
+                player.sendMessage ( StaffModeGUI2.getPlugin ().getConfig ().getString ( "defaultMessage.noPermission" ).replace ( "&" , "§" ) );
+            }
+            player.sendMessage ( StaffModeGUI2.getPlugin ().getConfig ().getString ( "gamemodeMenu.openGUI" ).replace ( "&" , "§" ) );
+            player.openInventory ( StaffModeGUI2.getInstance ().getGamemodeInv ().getInventory () );
+        } else if (event.getCurrentItem ().isSimilar ( MainInvItems.mobMenu () )) {
+            if (!player.hasPermission ( "staffmodegui.mobmenu" )) {
+                player.sendMessage ( StaffModeGUI2.getPlugin ().getConfig ().getString ( "defaultMessage.noPermission" ).replace ( "&" , "§" ) );
+            }
+            player.sendMessage ( StaffModeGUI2.getPlugin ().getConfig ().getString ( "mobMenu.openGUI" ).replace ( "&" , "§" ) );
+            player.openInventory ( StaffModeGUI2.getInstance ().getMobInv ().getInventory () );
+        } else if (event.getCurrentItem ().isSimilar ( MainInvItems.trollMenu () )) {
+            if (!player.hasPermission ( "staffmodegui.trollmenu" )) {
+                player.sendMessage ( StaffModeGUI2.getPlugin ().getConfig ().getString ( "defaultMessage.noPermission" ).replace ( "&" , "§" ) );
+            }
+            player.sendMessage ( StaffModeGUI2.getPlugin ().getConfig ().getString ( "trollMenu.openGUI" ).replace ( "&" , "§" ) );
+            player.openInventory ( StaffModeGUI2.getInstance ().getTrollInv ().getInventory () );
+        } else if (event.getCurrentItem ().isSimilar ( MainInvItems.toolsMenu () )) {
+            if (!player.hasPermission ( "staffmodegui.toolsmenu" )) {
+                player.sendMessage ( StaffModeGUI2.getPlugin ().getConfig ().getString ( "defaultMessage.noPermission" ).replace ( "&" , "§" ) );
+            }
+            player.sendMessage ( StaffModeGUI2.getPlugin ().getConfig ().getString ( "toolsMenu.openGUI" ).replace ( "&" , "§" ) );
+            player.openInventory ( StaffModeGUI2.getInstance ().getToolsInv ().getInventory () );
+        } else if (event.getCurrentItem ().isSimilar ( MainInvItems.serverManagerMenu () )) {
+            if (!player.hasPermission ( "staffmodegui.servermanagermenu" )) {
+                player.sendMessage ( StaffModeGUI2.getPlugin ().getConfig ().getString ( "defaultMessage.noPermission" ).replace ( "&" , "§" ) );
+            }
+            player.sendMessage ( StaffModeGUI2.getPlugin ().getConfig ().getString ( "serverManagerMenu.openGUI" ).replace ( "&" , "§" ) );
+            player.openInventory ( StaffModeGUI2.getInstance ().getServerManagerInv ().getInventory () );
+        } else if (event.getCurrentItem ().isSimilar ( MainInvItems.effectsMenu () )) {
+            if (!player.hasPermission ( "staffmodegui.effectsmenu" )) {
+                player.sendMessage ( StaffModeGUI2.getPlugin ().getConfig ().getString ( "defaultMessage.noPermission" ).replace ( "&" , "§" ) );
+            }
+            player.sendMessage ( StaffModeGUI2.getPlugin ().getConfig ().getString ( "effectsMenu.openGUI" ).replace ( "&" , "§" ) );
+            player.openInventory ( StaffModeGUI2.getInstance ().getEffectsInv ().getInventory () );
         }
-    }
-
-
-    public static StaffModeGUI2 getInstance() {
-        return instance;
-    }
-
-
-    //INV
-
-    public MainInv getMainInv() {
-        return mainInv;
-    }
-
-    public WhitelistInv getWhitelistInv() {
-        return whitelistInv;
-    }
-
-    public TimeInv getTimeInv() {
-        return timeInv;
-    }
-
-    public WeatherInv getWeatherInv() {
-        return weatherInv;
-    }
-
-    public GamemodeInv getGamemodeInv() {
-        return gamemodeInv;
-    }
-
-    public FlyInv getFlyInv() {
-        return flyInv;
-    }
-
-    public VanishInv getVanishInv() {
-        return vanishInv;
-    }
-
-    public MobInv getMobInv() {
-        return mobInv;
-    }
-
-    public ExtraMobInv getExtraMobInv() {
-        return extraMobInv;
-    }
-
-    public TrollInv getTrollInv() {
-        return trollInv;
-    }
-
-    public ToolsInv getToolsInv() {
-        return toolsInv;
-    }
-
-    public ServerManagerInv getServerManagerInv() {
-        return serverManagerInv;
-    }
-
-    public FeedAndHealInv getFeedAndHealInv() {
-        return feedAndHealInv;
-    }
-
-    public EffectsInv getEffectsInv() {
-        return effectsInv;
-    }
-
-    public SpeedInv getSpeedInv() {
-        return speedInv;
-    }
-
-    public StrengthInv getStrengthInv() {
-        return strengthInv;
-}
-
-    public JumpboostInv getJumpboostInv() {
-        return jumpboostInv;
     }
 }
