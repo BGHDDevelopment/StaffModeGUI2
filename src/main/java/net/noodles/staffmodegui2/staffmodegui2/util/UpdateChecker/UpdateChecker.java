30e152fd2f6d03497dcc4d5a9eaaf790d8e7b988
Refrac <31602472+Refrac@users.noreply.github.com>
2020-01-12 17:04:39 -0600

    Update UpdateChecker.java

--- src/main/java/net/noodles/staffmodegui2/staffmodegui2/util/UpdateChecker/UpdateChecker.java
+++ src/main/java/net/noodles/staffmodegui2/staffmodegui2/util/UpdateChecker/UpdateChecker.java
@@ -18,19 +18,20 @@ public class UpdateChecker {
     private StaffModeGUI2 plugin;
     private int resourceId;
 
-    public UpdateChecker(StaffModeGUI2 plugin, int resourceId) {
+    public UpdateChecker(StaffModeGUI2 plugin , int resourceId) {
         this.plugin = plugin;
         this.resourceId = resourceId;
     }
 
     public void getLatestVersion(Consumer<String> consumer) {
-        Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
-            try (InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId).openStream(); Scanner scanner = new Scanner(inputStream)) {
-                if (scanner.hasNext()) {
-                    consumer.accept(scanner.next());
+        Bukkit.getScheduler ().runTaskAsynchronously ( this.plugin , () -> {
+            try (InputStream inputStream = new URL ( "https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId ).openStream (); Scanner scanner = new Scanner ( inputStream )) {
+                if (scanner.hasNext ()) {
+                    consumer.accept ( scanner.next () );
                 }
             } catch (IOException exception) {
-                this.plugin.getLogger().info("Cannot look for updates: " + exception.getMessage());
+                this.plugin.getLogger ().info ( "Cannot look for updates: " + exception.getMessage () );
             }
-        });
+        } );
     }
+}
